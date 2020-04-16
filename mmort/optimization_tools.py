251f3cab89df0b3d7a565a5c4d19355c2770d9aa
LevinRoman <romalevi@yandex.ru>
2020-04-16 13:26:57 -0700

    Rollback from smoothing attempt

--- mmort/optimization_tools.py
+++ mmort/optimization_tools.py
@@ -186,7 +186,8 @@ def relaxed_obj_u_opt_N_fixed(u, w_0, w, eta_0, eta, T, H, alpha, B):
         w_0-tumor_dose))**2 + np.sum([(1/(2*eta[i]))*(np.linalg.norm(w[i]-OAR_doses[i]))**2 for i in range(len(H))])
     return relaxed_obj
 
-
+#############################################################################################
+#Review this one, it is not a function of N!
 def obj_u_opt_N_opt(u, T, alpha, B, N, num_tumor_voxels, Td = 2):
     """Objective function with N in tumor doubling and with N implicit in modality coefficients.
     Based on the paper "Proof of Principle: Multi-Modality Radiotherapy Optimization",
@@ -232,7 +233,7 @@ def obj_u_opt_N_opt(u, T, alpha, B, N, num_tumor_voxels, Td = 2):
     B_tilde = B #np.repeat(N, num_tumor_voxels)*B
     #Note that all modalities must have the same number of tumor voxels:
     return alpha_tilde.T.dot(x) - x.T.dot(B_tilde*x) + num_tumor_voxels*(np.sum(N)-1)*(np.log(2)/Td)
-
+###########################################################################################################
 
 def constraint(u, H, gamma, D, C, tol, verbose = 0):
     """Check the constraint violation for given u, fixed N
@@ -320,43 +321,113 @@ def constraints_all(u, H, gamma, D, C, tol = 0.05, verbose = 0):
 
 
 #update in u:
-def u_update(eta_0, eta, w_0, w, eta_T_H_stacked, premultiplied_lhs = None, nnls_max_iter=50):
-    """Compute the sparse least squares update for u per section 3.2.1 of the paper
-    The rhs of the ls problem needs to be recomputed every time since w_0 and w are variables
-    The lhs should be precomputed in advance for computational efficiency
+#The following is an attempt to incorporate smoothing
+# def u_update(eta_s, eta_0, eta, w_s, w_0, w, eta_S_T_H_stacked, premultiplied_lhs = None, nnls_max_iter=50):
+#     """Compute the sparse least squares update for u per section 3.2.1 of the paper
+#     The rhs of the ls problem needs to be recomputed every time since w_0 and w are variables
+#     The lhs should be precomputed in advance for computational efficiency
 
-    Parameters
-    ----------
-    eta_0 : float
-        Penalty parameter for the tumor
-    eta : np.array of shape (None,)
-        Array of OAR penalty parameters
-    w_0 : np.array of shape (None,)
-        Auxilary dose distribution, optimal for the tumor
-    w : List of np.arrays of shape (None,)
-        Auxilary dose distributions feasible for OARs
-    eta_T_H_stacked : Sparse array of shape (None, None)
-        Stacked dose deposition matrices, premultiplied by penalty parameters, needs to be precomputed in the solver, 
-        o.w. expensive to do at every iteration, lhs of the least squares update
-    premultiplied_lhs : Dense array of shape (None, None)
-        A.T@A for the nnls, old solver
-    nnls_max_iter : int
-        Max iterations for the internal scipy nnls
+#     Parameters
+#     ----------
+#     eta_0 : float
+#         Penalty parameter for the tumor
+#     eta : np.array of shape (None,)
+#         Array of OAR penalty parameters
+#     w_0 : np.array of shape (None,)
+#         Auxilary dose distribution, optimal for the tumor
+#     w : List of np.arrays of shape (None,)
+#         Auxilary dose distributions feasible for OARs
+#     eta_T_H_stacked : Sparse array of shape (None, None)
+#         Stacked dose deposition matrices, premultiplied by penalty parameters, needs to be precomputed in the solver, 
+#         o.w. expensive to do at every iteration, lhs of the least squares update
+#     premultiplied_lhs : Dense array of shape (None, None)
+#         A.T@A for the nnls, old solver
+#     nnls_max_iter : int
+#         Max iterations for the internal scipy nnls
         
-    Returns
-    -------
-    u_next : np.array of shape (None,)
-        update for the beamlet radiation distribution u
+#     Returns
+#     -------
+#     u_next : np.array of shape (None,)
+#         update for the beamlet radiation distribution u
         
-    Notes
-    -----
-    Changes: Took the lhs computation out of the update to make it more efficient
-    TODO: Add a check for sparsity? Fix the hack with [[]], add the option to choose solvers
-    """
-    """In the following  +[[]] and [:-1] are added to keep thing 1dim array of objects and still multiply it elemtwisely"""
-    w_concat = np.concatenate((1/np.sqrt(2*eta))*np.array(w+[[]])[:-1], axis = 0) #[:-1] Added as a hack to keep it one-dim array of objects
-    b_ls = np.concatenate([(1/np.sqrt(2*eta_0))*w_0, w_concat], axis = 0)
-    u_next = scipy.optimize.lsq_linear(eta_T_H_stacked, b_ls, bounds = (0, np.inf), tol=1e-4, lsmr_tol=1e-1, max_iter=nnls_max_iter, verbose=1).x
+#     Notes
+#     -----
+#     Changes: Took the lhs computation out of the update to make it more efficient
+#     TODO: Add a check for sparsity? Fix the hack with [[]], add the option to choose solvers
+#     """
+#     """In the following  +[[]] and [:-1] are added to keep 1dim array of objects and still multiply it elemtwisely"""
+#     w_concat = np.concatenate((1/np.sqrt(2*eta))*np.array(w+[[]])[:-1], axis = 0) #[:-1] Added as a hack to keep it one-dim array of objects
+#     b_ls = np.concatenate([(1/np.sqrt(2*eta_s))*w_s, (1/np.sqrt(2*eta_0))*w_0, w_concat], axis = 0)
+#     u_next = scipy.optimize.lsq_linear(eta_S_T_H_stacked, b_ls, bounds = (0, np.inf), tol=1e-4, lsmr_tol=1e-1, max_iter=nnls_max_iter, verbose=1).x
+#     return u_next
+
+
+
+def u_update(eta_0, eta, w_0, w, eta_T_H_stacked, premultiplied_lhs = None, nnls_max_iter=50):  
+    # PREMULTIPLIED LHS IS AN EXTRA ARGUMENT! Set it to None and add solver!    
+    """Compute the sparse least squares update for u per section 3.2.1 of the paper 
+    The rhs of the ls problem needs to be recomputed every time since w_0 and w are variables   
+    The lhs should be precomputed in advance for computational efficiency   
+    
+    Parameters  
+    ----------  
+    eta_0 : float   
+        Penalty parameter for the tumor 
+    eta : np.array of shape (None,) 
+        Array of OAR penalty parameters 
+    w_0 : np.array of shape (None,) 
+        Auxilary dose distribution, optimal for the tumor   
+    w : List of np.arrays of shape (None,)  
+        Auxilary dose distributions feasible for OARs   
+    eta_T_H_stacked : Sparse array of shape (None, None)    
+        Stacked dose deposition matrices, premultiplied by penalty parameters, needs to be precomputed in the solver,   
+        o.w. expensive to do at every iteration, lhs of the least squares update    
+    premultiplied_lhs : Dense array of shape (None, None)   
+        A.T@A for the nnls, old solver  
+    nnls_max_iter : int 
+        Max iterations for the internal scipy nnls  
+            
+    Returns 
+    ------- 
+    u_next : np.array of shape (None,)  
+        update for the beamlet radiation distribution u 
+            
+    Notes   
+    -----   
+    Changes: Took the lhs computation out of the update to make it more efficient   
+    TODO: Add a check for sparsity? Fix the hack with [[]], add the option to choose solvers    
+    Another idea: use u = nnls_predotted(premultiplied_lhs, premultiplied_rhs, tol=1e-8)    
+                  with premultiplied_lhs precomputed outside the solver loop, then uncomment the nnls   
+                  (create a separate file with it)  
+    """ 
+    """In the following  +[[]] and [:-1] are added to keep thing 1dim array of objects and still multiply it elemtwisely""" 
+#     #B.append([]) #THIS IS WRONG, CHANGES THE LIST    
+#     B_concat = np.concatenate((1/np.sqrt(2*eta))*np.array(B+[[]])[:-1], axis = 0) 
+#     A_ls = np.concatenate([(1/np.sqrt(2*eta0))*A, B_concat], axis = 0)    
+#     #print(np.array(B).shape) 
+#     #print(w[0].shape)    
+#     #print(w, eta)    
+#     #w.append([]) THIS IS WRONG, CHANGES THE LIST 
+#     w_concat = np.concatenate((1/np.sqrt(2*eta))*np.array(w+[[]])[:-1], axis = 0) #[:-1] Added as a hack to keep it one-dim array of objects  
+#     eta_w = np.expand_dims(1/np.sqrt(2*eta),1)*np.array(w)    
+#     print(eta_w.shape)    
+#     b_ls = np.concatenate([(1/np.sqrt(2*eta_0))*w_0, eta_w.flatten()], axis = 0)  
+    w_concat = np.concatenate((1/np.sqrt(2*eta))*np.array(w+[[]])[:-1], axis = 0) #[:-1] Added as a hack to keep it one-dim array of objects    
+    b_ls = np.concatenate([(1/np.sqrt(2*eta_0))*w_0, w_concat], axis = 0)   
+#     print(np.sum(eta_w.flatten() != w_concat))    
+#     premultiplied_time_start = time.time()    
+#     premultiplied_lhs = eta_T_H_stacked.T.dot(eta_T_H_stacked).toarray()  
+#     premultiplied_time_end = time.time()  
+#     print('premultiplying took {}'.format(premultiplied_time_end - premultiplied_time_start)) 
+#     premultiplied_rhs = eta_T_H_stacked.T.dot(b_ls)   
+#     u_next = nnls_predotted(premultiplied_lhs, premultiplied_rhs, tol=1e-5)   
+#     print(eta_T_H_stacked.shape, b_ls.shape)  
+#     A_ls_t_b = eta_T_H_stacked.T.dot(b_ls)    
+#     w =scipy.sparse.linalg.spsolve_triangular(RT, A_ls_t_b, lower = True) 
+#     x = scipy.sparse.linalg.spsolve_triangular(R, w, lower = False)   
+#     u_next = x    
+    u_next = scipy.optimize.lsq_linear(eta_T_H_stacked, b_ls, bounds = (0, np.inf), tol=1e-4, lsmr_tol=1e-1, max_iter=nnls_max_iter, verbose=1).x   
+#     u = scipy.optimize.lsq_linear(premultiplied_lhs, premultiplied_rhs, bounds = (0, np.inf), tol=1e-5).x 
     return u_next
 
 #update in w0:
@@ -418,11 +489,120 @@ def w_update(u, H, gamma, D, C):
     w_next = [proj(H[i].dot(u), gamma[i], D[i], C[i]) for i in range(len(H))]
     return w_next
 
+#Update for w_smoothing
+def w_s_update(u, S):
+    """Computes the updates for the proxy of the derivative of u for smoothing
+
+    Parameters
+    ----------
+    u : np.array of shape (None,)
+        Beamlet radiation distribution u
+    S : np.array of shape (None, None)   
+    Returns
+    -------
+    w_next : list of np.array of shape (None,)
+        updates for generalized OAR auxilary dose distributions
+
+    
+    """
+    w_next = S.dot(u)
+    w_next[w_next>0] = 0
+    return w_next
+
+
+
 
 
 #SOLVER:
 
-#for given N1, N2, eta0, eta
+# The following is an attempt to incorporate smoothing matrix
+# #for given N1, N2, eta0, eta
+# def solver(u_init, eta_s, eta_0, eta, S, T, H, alpha, gamma, B, D, C, ftol = 1e-3, max_iter = 5000, verbose = 0):
+#     """Returns the optimal u for the relaxed problem in section 3.2.1 of the paper
+
+#     Parameters
+#     ----------
+#     u_init : np.array of shape (None,)
+#         Initial guess for the beamlet radiation distribution u
+#     eta_0 : float
+#         Penalty parameter for the tumor
+#     eta : np.array of shape (None,)
+#         Array of OAR penalty parameters
+#     T : Sparse array of shape(None, None)
+#         Block-diagonal tumor dose-deposition matrix block_diag(T_1, ... , T_M) 
+#         where T_i is the sparse dose deposition matrix for i-th modality
+#     H : List of sparse arrays of shape(None, None)
+#         List of block-diagonal generalized OAR dose-deposition matrices block_diag(H_1, ... , H_M) 
+#         where H_i is the sparse dose deposition matrix for i-th modality for a given OAR
+#     alpha : np.array of shape (None,)
+#         Linear block coefficient array of the tumor BE 
+#         (with N_m included as coefficients)
+#     gamma : List of np.arrays of shape (None,)
+#         List of linear block coefficient array of the generalized OAR BE 
+#         (with N_m included as coefficients)
+#     B : np.array of shape (None,)
+#         Quadratic block-diagonal coefficient array B of the tumor BE
+#         (with N_m included as coefficients)
+#     D : List of np.arrays of shape (None,)
+#         List of quadratic block-diagonal coefficient arrays D of the generalized OAR BEs
+#         (with N_m included as coefficients)
+#     C : array-like of floats
+#         Constraints to satisfy
+#     ftol : float
+#         Relative reduction in the objective value for the stopping criterium
+#     max_iter : int
+#         Max number of iterations
+#     verbose : 0 or 1
+#         0 for no printing
+#         1 for printing
+   
+#     Returns
+#     -------
+#     u : np.array of shape (None,)
+#         The optimal u for the relaxed problem in section 3.2.1 of the paper
+    
+#     """
+#     #precompute the expensive operation:
+#     eta_S_T_H_stacked = scipy.sparse.vstack([S.multiply(1/np.sqrt(2*eta_s))] + [T.multiply(1/np.sqrt(2*eta_0))] + [H[i].multiply(1/np.sqrt(2*eta[i])) for i in range(len(H))])
+#     u_prev = u_init + 1
+#     u = u_init
+#     count = 0
+#     obj_history = []
+#     relaxed_obj_history = [-1, 0.1] #just two initial values to enter the loop
+#     while np.abs((relaxed_obj_history[-2] - relaxed_obj_history[-1])/relaxed_obj_history[-2]) > ftol and count < max_iter:
+#         start = time.time()
+        
+#         u_prev = np.copy(u)
+#         w_s = w_s_update(u, S)
+#         w_0 = w_0_update(eta_0, u, T, alpha, B) 
+#         w = w_update(u, H, gamma, D, C) 
+
+#         u = u_update(eta_s, eta_0, eta, w_s, w_0, w, eta_S_T_H_stacked, nnls_max_iter=30)
+
+#         # u = u_update(eta_0, eta, w_s, w_0, w, eta_S_T_H_stacked, nnls_max_iter=30)
+
+#         count += 1 
+#         if count == 10:
+#             u_inf = np.copy(u)
+#         if count > 10 and np.abs(cur_obj) > 1e+15:
+#             print('INFINITY! RETURNING u at the 10-th iteration to enter the feasibility loop')
+#             return u_inf, obj_history, relaxed_obj_history
+        
+#         cur_obj = obj_u_opt_N_fixed(u, T, alpha, B)
+#         obj_history.append(cur_obj)
+#         cur_relaxed_obj = relaxed_obj_u_opt_N_fixed(u, w_0, w, eta_0, eta, T, H, alpha, B)
+#         relaxed_obj_history.append(cur_relaxed_obj)    
+        
+#         stop = time.time()
+#         duration = stop-start
+        
+#         if count%1 == 0 and verbose: 
+#             stopping_criterium = np.abs((relaxed_obj_history[-2] - relaxed_obj_history[-1])/relaxed_obj_history[-2])
+#             print('    iter = {}, stopping criterium:{}, OBJ {}'.format(count, stopping_criterium, cur_obj))
+#             print('    This iteration took: {}'.format(duration))
+#     return u, obj_history, relaxed_obj_history
+
+
 def solver(u_init, eta_0, eta, T, H, alpha, gamma, B, D, C, ftol = 1e-3, max_iter = 5000, verbose = 0):
     """Returns the optimal u for the relaxed problem in section 3.2.1 of the paper
 
@@ -470,24 +650,28 @@ def solver(u_init, eta_0, eta, T, H, alpha, gamma, B, D, C, ftol = 1e-3, max_ite
     """
     #precompute the expensive operation:
     eta_T_H_stacked = scipy.sparse.vstack([T.multiply(1/np.sqrt(2*eta_0))] + [H[i].multiply(1/np.sqrt(2*eta[i])) for i in range(len(H))])
+    #!!!!
+#     premultiplied_lhs = eta_T_H_stacked.T.dot(eta_T_H_stacked).toarray()
+    #!!!!
     u_prev = u_init + 1
     u = u_init
     count = 0
     obj_history = []
     relaxed_obj_history = [-1, 0.1] #just two initial values to enter the loop
-    while np.abs((relaxed_obj_history[-2] - relaxed_obj_history[-1])/relaxed_obj_history[-2]) > ftol and count < max_iter:
+    while np.abs((relaxed_obj_history[-2] - relaxed_obj_history[-1])/relaxed_obj_history[-2]) > ftol and count < max_iter:#np.linalg.norm(u - u_prev, np.inf) > 1e-3 and count < max_iter: #Maybe all of them stop changing
         start = time.time()
         
         u_prev = np.copy(u)
         w_0 = w_0_update(eta_0, u, T, alpha, B) 
         w = w_update(u, H, gamma, D, C) 
-
+#         u = u_update(eta_0, eta, w_0, w, eta_T_H_stacked, nnls_max_iter=50)
+        #!!!!
         u = u_update(eta_0, eta, w_0, w, eta_T_H_stacked, nnls_max_iter=30)
-
+        #!!!!
         count += 1 
         if count == 10:
             u_inf = np.copy(u)
-        if count > 10 and np.abs(cur_obj) > 1e+15:
+        if count > 10 and np.abs(cur_obj) > 1e+15: #HANDLE THIS BETTER!!!
             print('INFINITY! RETURNING u at the 10-th iteration to enter the feasibility loop')
             return u_inf, obj_history, relaxed_obj_history
         
