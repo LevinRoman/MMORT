69735d839e8d3021c8ee362c48f5ba4100f5ffe3
LevinRoman <romalevi@yandex.ru>
2020-12-05 22:28:25 -0800

    investigating condition number of A

--- mmort/optimization_tools.py
+++ mmort/optimization_tools.py
@@ -517,8 +517,18 @@ def u_update(u_cur, AtA, AA, S, StS, lambda_smoothing, eta_0, eta, w_0, w, eta_T
     b = b_ls
     Atb = A.T.dot(b)
     lambda_smoothing_ = np.copy(lambda_smoothing) #To avoid changing it inplace
-    print('\n Condition number of A prior to renormalization:', np.linalg.cond(A.toarray()))
-    print('\n Condition number of A prior to renormalization:', np.linalg.matrix_rank(A.toarray()))
+    # print('\n Condition number of A prior to renormalization:', np.linalg.cond(A.toarray()))
+    # print('\n Condition number of A prior to renormalization:', np.linalg.matrix_rank(A.toarray()))
+
+    #Let's try preconditioning:
+    precond = np.linalg.pinv(A)
+    A = precond.dot(A)
+    b = precond.dot(b)
+    AA = A.T.dot(A)
+    Atb = A.T.dot(b)
+    # print('\')
+    print('\n Preconditioned. New condition number of A:'np.linalg.cond(A))
+
     
     if normalize:
         normalization = 1/np.max(1/(2*eta))
