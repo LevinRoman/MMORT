61a5632fd2e999e7a9959dae253e14ec6b7fc353
Levin <romalevi@yandex.ru>
2021-02-22 10:22:12 -0800

    Adding IMRT jobs (commented)

--- scripts/launch/IMRT_jobs.sh
+++ scripts/launch/IMRT_jobs.sh
@@ -3,13 +3,13 @@
 # python3 compute_solution.py --config_experiment 'Experiment_2' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'no' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
 
 #d = 85
-python3 compute_solution.py --config_experiment 'Experiment_3' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'no' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
+python3 compute_solution.py --config_experiment 'Experiment_3' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'yes' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
 
 #d = 60
-python3 compute_solution.py --config_experiment 'Experiment_4' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'no' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
+python3 compute_solution.py --config_experiment 'Experiment_4' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'yes' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
 
 #d = 100
-python3 compute_solution.py --config_experiment 'Experiment_5' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'no' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
+python3 compute_solution.py --config_experiment 'Experiment_5' --N_photon 44.0 --compute_mult 'no' --compute_proton 'no' --compute_photon 'yes'  --N1 44.0 --N2 0.0 --Rx 80.0 --precomputed_input 'yes' --lambda_smoothing 0.1 --eta_coef_photon 10000000 --enforce_smooth_u --IMRT
 
 #Evaluation
 #d = 81
