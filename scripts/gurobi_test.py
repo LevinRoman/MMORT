d8b35f2eab2de8d43f75b94a683a53a5115a0234
Levin <romalevi@yandex.ru>
2021-04-05 19:22:51 -0700

    adding projected gradient descent through gurobi

--- scripts/gurobi_test.py
+++ scripts/gurobi_test.py
@@ -22,14 +22,13 @@ from config import configurations
 import gurobipy as gp
 from gurobipy import GRB
 
-data_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'data', 'ProstateExample_BODY_not_reduced_with_OAR_constraints.mat'))
-# data_no_body_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'data', 'ProstateExample.mat'))
-data = scipy.io.loadmat(data_path)
-# data_no_body =  scipy.io.loadmat(data_no_body_path)
+# data_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'data', 'ProstateExample_BODY_not_reduced_with_OAR_constraints.mat'))
+data_no_body_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'data', 'ProstateExample.mat'))
+# data = scipy.io.loadmat(data_path)
+data =  scipy.io.loadmat(data_no_body_path)
 Alpha = np.array([0.35, 0.35])
 Beta = np.array([0.175, 0.175])
 Gamma = np.array([np.array([0.35, 0.35]),
-				  np.array([0.35, 0.35]),
 				  np.array([0.35, 0.35]),
 				  np.array([0.35, 0.35]),
 				  np.array([0.35, 0.35])               
@@ -37,16 +36,16 @@ Gamma = np.array([np.array([0.35, 0.35]),
 Delta = np.array([np.array([0.07, 0.07]),
 				  np.array([0.07, 0.07]),
 				  np.array([0.175, 0.175]),
-				  np.array([0.175, 0.175]),
 				  np.array([0.175, 0.175])                
 				 ])
 modality_names = np.array(['Aphoton', 'Aproton'])
 
-num_body_voxels = 683189
-data['Aphoton'][-1] = data['Aphoton'][-1]/num_body_voxels
-data['Aproton'][-1] = data['Aproton'][-1]/num_body_voxels
+# num_body_voxels = 683189
+# data['Aphoton'][-1] = data['Aphoton'][-1]/num_body_voxels
+# data['Aproton'][-1] = data['Aproton'][-1]/num_body_voxels
 
-
+for modality in modality_names:
+        data[modality] = scipy.sparse.csr_matrix(data[modality])
 # # Create a new model
 # m = gp.Model("qp")
 
@@ -102,17 +101,19 @@ data['Aproton'][-1] = data['Aproton'][-1]/num_body_voxels
 # B_photon = utils.load_obj('B_photon', loading_dir)
 # D_photon = utils.load_obj('D_photon', loading_dir)
 # C_photon = utils.load_obj('C_photon', loading_dir)
-def fixed_N_qcqp(N, dose_deposition_dict, constraint_dict, radbio_dict, S, alpha_smoothing):
+def u_projection_fixed_N(u_to_project, N, dose_deposition_dict, constraint_dict, radbio_dict, S, u_max = 1000):
 	"""In every dict, keys are organ namesSo far, we are doing this for photons onlyS is the smoothing matrix"""
 	T = dose_deposition_dict['Target']
-	m = gp.Model("Fixed-N-QCQP")
-	m.params.NonConvex = 2
-	u = m.addMVar(T.shape[1], vtype=GRB.CONTINUOUS, lb=0.0, name="u")
+	m = gp.Model("rojection-convex-QCQP")
+	# m.params.NonConvex = 2
+	u = m.addMVar(T.shape[1], vtype=GRB.CONTINUOUS, lb=0.0, ub = u_max, name="u")
 	
-	target_dose = T@u #Dose
-	smoothing_term = S@u
-	alpha, beta = radbio_dict['Target'] #Linear and quadratic coefficients
-	obj = -N*((alpha*np.ones(T.shape[0])@T)@u + u@(beta*T.T@T)@u) + u@(alpha_smoothing*S.T@S)@u
+	# target_dose = T@u #Dose
+	# smoothing_term = S@u
+	# alpha, beta = radbio_dict['Target'] #Linear and quadratic coefficients
+	# obj = -N*((alpha*np.ones(T.shape[0])@T)@u + u@(beta*T.T@T)@u) #+ u@(alpha_smoothing*S.T@S)@u
+	difference_vector = u - u_to_project
+	obj = difference_vector@difference_vector
 	m.setObjective(obj)
 
 	OAR_names = list(dose_deposition_dict.keys())[1:] #Not including Target
@@ -135,47 +136,53 @@ def fixed_N_qcqp(N, dose_deposition_dict, constraint_dict, radbio_dict, S, alpha
 			#The sum of BEs across voxels
 			mean_constr = N*((gamma*np.ones(H.shape[0])@H)@u + u@(delta*H.T@H)@u)
 			m.addConstr(mean_constr <= mean_constraint_BE, "{} max constraint".format(oar))
-
+	#add smoothing constraint
+	m.addConstr(S@u <= 0)
 	m.optimize()
 	for v in m.getVars():
 		print('%s %g' % (v.varName, v.x))
 
 	print('Obj: %g' % obj.getValue())
-	utils.save_obj(u.x, 'u_photon_gurobi', '')
+	utils.save_obj(u.X, 'u_photon_gurobi', '')
 	print('Saved solution')
-	return
+	print(u.X)
+	return u.X
 
-dose_deposition_dict = {}
-constraint_dict = {}
-radbio_dict = {}
+def create_coefficient_dicts(data):
+	"""So far only creates coefficients for the first modality"""
+	dose_deposition_dict = {}
+	constraint_dict = {}
+	radbio_dict = {}
 
-organ_names = [str(i[0]) for i in np.squeeze(data['Organ'])]
-len_voxels = data['Aphoton'].shape[0]
-#[:-1] because we don't wabt the last isolated voxel
-organ_indices = np.split(np.arange(len_voxels), np.cumsum(np.squeeze(data['num_voxels'])))[:-1]
-OAR_constr_types = np.squeeze(data['OAR_constraint_types'])
-OAR_constr_values = np.squeeze(data['OAR_constraint_values'])
-for organ_name in organ_names:
-	organ_number = organ_names.index(organ_name)
-	oar_number = organ_number - 1 #Because Target is also an organ
-	dose_deposition_dict[organ_name] = data['Aphoton'][organ_indices[organ_number]]
-	if organ_name == 'Target':
-		radbio_dict[organ_name] = Alpha[0], Beta[0] #So far, only photons
-	if organ_name != 'Target':
-		constraint_type = OAR_constr_types[oar_number].strip()
-		constraint_dose = OAR_constr_values[oar_number]
-		constraint_N = 44
-		constraint_dict[organ_name] = (constraint_type, constraint_dose, constraint_N)
-		radbio_dict[organ_name] = Gamma[oar_number][0], Delta[oar_number][0]
-
+	organ_names = [str(i[0]) for i in np.squeeze(data['Organ'])]
+	len_voxels = data['Aphoton'].shape[0]
+	#[:-1] because we don't want the last isolated voxel
+	organ_indices = np.split(np.arange(len_voxels), np.cumsum(np.squeeze(data['num_voxels'])))[:-1]
+	OAR_constr_types = np.squeeze(data['OAR_constraint_types'])
+	OAR_constr_values = np.squeeze(data['OAR_constraint_values'])
+	for organ_number, organ_name in enumerate(organ_names):
+		oar_number = organ_number - 1 #Because Target is also an organ
+		dose_deposition_dict[organ_name] = data['Aphoton'][organ_indices[organ_number]]
+		if organ_name == 'Target':
+			radbio_dict[organ_name] = Alpha[0], Beta[0] #So far, only photons
+		if organ_name != 'Target':
+			constraint_type = OAR_constr_types[oar_number].strip()
+			constraint_dose = OAR_constr_values[oar_number]
+			constraint_N = 44
+			constraint_dict[organ_name] = constraint_type, constraint_dose, constraint_N
+			radbio_dict[organ_name] = Gamma[oar_number][0], Delta[oar_number][0]
+return dose_deposition_dict, constraint_dict, radbio_dict
 
 N = 44
 #Set up smoothing matrix
 len_voxels = data['Aphoton'].shape[0]
 beamlet_indices = np.split(np.arange(len_voxels), np.cumsum(np.squeeze(data['num_beamlets'])))[:-1] 
 beams = [data['beamlet_pos'][i] for i in beamlet_indices]
-S = utils.construct_smoothing_matrix(beams, eps = 5)
-alpha_smoothing = 1e-2
+S = utils.construct_smoothing_matrix_relative(beams, 0.25, eps = 5)
+
+
+dose_deposition_dict, constraint_dict, radbio_dict = create_coefficient_dicts(data)
+
 print('\nDose_deposition_dict:', dose_deposition_dict)
 print('\nConstraint dict:', constraint_dict)
 print('\nradbio_dict:', radbio_dict)
@@ -183,7 +190,11 @@ print('\nN:', N)
 print('\nS shape:', S.shape)
 
 print('\n Running optimization...')
-fixed_N_qcqp(N, dose_deposition_dict, constraint_dict, radbio_dict, S, alpha_smoothing)
+Rx = 81
+LHS1 = data['Aphoton']
+RHS1 = np.array([Rx/N]*LHS1.shape[0])
+u_to_project = scipy.optimize.lsq_linear(LHS1, RHS1, bounds = (0, np.inf), tol=1e-4, lsmr_tol=1e-4, max_iter=100, verbose=1).x
+u_projection_fixed_N(u_to_project, N, dose_deposition_dict, constraint_dict, radbio_dict, S, u_max = 1000)
 
 
 
